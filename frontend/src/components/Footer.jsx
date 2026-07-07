d0dac02429a0495ac22ccd0963c5b8e9fe4da2e8
DeibyVargasChaves <dvargas30884@ufide.ac.cr>
2026-07-06 18:37:32 -0600

    Cambio de nombre del footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
     return (
         <footer className="border-t border-slate-700 py-6 px-5">
             <p className="text-slate-400 text-sm text-center">
-                © {new Date().getFullYear()} DeliveryApp. Todos los derechos reservados Grupo 5 - Lunes -  CS-601 .
+                © {new Date().getFullYear()} DeUna CR. Todos los derechos reservados Grupo 5 - Lunes -  CS-601 .
             </p>
         </footer>
     )
