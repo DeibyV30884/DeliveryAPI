f2816f3607f3c1fbb335127e8f6a57102664199f
JuanChok09 <juansnook123@gmail.com>
2026-07-18 21:56:29 -0600

    Corregir acceso del rol Admin al panel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -80,7 +80,7 @@ function App() {
                     <Route
                         path="/administrador"
                         element={
-                            <RutaProtegida rolesPermitidos={['Administrador']}>
+                            <RutaProtegida rolesPermitidos={['Admin', 'Administrador']}>
                                 <PanelAdministrador />
                             </RutaProtegida>
                         }
--- frontend/src/utils/rutasPorRol.js
+++ frontend/src/utils/rutasPorRol.js
@@ -2,5 +2,6 @@ export const rutasPorRol = {
     Cliente: '/cliente',
     Restaurante: '/restaurante',
     Repartidor: '/repartidor',
+    Admin: '/administrador',
     Administrador: '/administrador',
 }
